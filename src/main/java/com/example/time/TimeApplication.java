00b9d79c4b934cc3f9f768b6d3248d948fed419d
Johan Navarro <21sebas12@gmail.com>
2020-04-17 08:41:30 -0500

    exlude JACKSON form spring

--- src/main/java/com/example/time/TimeApplication.java
+++ src/main/java/com/example/time/TimeApplication.java
@@ -2,9 +2,10 @@ package com.example.time;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {JacksonAutoConfiguration.class})
 @EnableConfigurationProperties
 public class TimeApplication {
 
