fccd92e77e3bb8e1479f091c10b8bb5a7472ee11
Johan Navarro <21sebas12@gmail.com>
2020-04-17 09:43:10 -0500

    se establecen las condiciones para la Unidad de tiempo y se hace una forma sencilla la comparacion de los Strings

--- src/main/java/com/example/time/horaDia/domain/UnidadTiempo.java
+++ src/main/java/com/example/time/horaDia/domain/UnidadTiempo.java
@@ -1,4 +1,25 @@
 package com.example.time.horaDia.domain;
 
+import com.example.time.common.Preconditions;
+
+import java.util.regex.Pattern;
+
 public class UnidadTiempo {
+
+    public static UnidadTiempo of(String value){
+        return new UnidadTiempo(value);
+    }
+
+
+    private final String value;
+
+    private UnidadTiempo(String value) {
+        Preconditions.checkNotNull(value);
+        Preconditions.checkAgument(value.matches("Milisegundos|Segundos|Minutos|Horas"));
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
