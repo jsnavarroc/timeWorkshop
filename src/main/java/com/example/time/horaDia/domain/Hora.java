4ffc9cf6a074760a98fc66184299b81ac73e54ce
Johan Navarro <21sebas12@gmail.com>
2020-04-17 09:21:36 -0500

    se establecen los controles para el ValueObject de la Hora con la clase Precondiciones

--- src/main/java/com/example/time/horaDia/domain/Hora.java
+++ src/main/java/com/example/time/horaDia/domain/Hora.java
@@ -1,5 +1,6 @@
 package com.example.time.horaDia.domain;
 
+import com.example.time.common.Preconditions;
 import lombok.Value;
 
 @Value(staticConstructor = "of")
@@ -7,6 +8,8 @@ public class Hora {
     private final Integer value;
 
     private Hora(Integer value) {
+        Preconditions.checkNotNull(value);
+        Preconditions.checkAgument(value >= 0 && value <= 23);
         this.value = value;
     }
 }
