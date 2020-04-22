bd9d8b7526858a904277829224bff7231ba684a8
Johan Navarro <21sebas12@gmail.com>
2020-04-22 11:49:09 -0500

    se prueba que el adapatador funciona de manera correcta

--- src/main/java/com/example/time/Prueba.java
+++ src/main/java/com/example/time/Prueba.java
@@ -1,23 +1,37 @@
 package com.example.time;
 
-import com.example.time.configuration.domain.DatabaseCredentials;
-import com.example.time.configuration.domain.UnidadTiempoTipo;
+
 import com.example.time.horaDia.domain.Hora;
-import com.example.time.horaDia.domain.HoraDia;
+import com.example.time.horaDia.domain.Minuto;
+import com.example.time.horaDia.domain.Segundo;
 import com.example.time.horaDia.domain.UnidadTiempoEnum;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-import java.time.LocalTime;
+
+import java.util.function.Function;
 
 @EnableConfigurationProperties
 public class Prueba {
-    @Value("${horadia}")
-    private String unidadtiempo;
-
+    public static Hora creator(Integer value, Function<Integer, Hora> creator){
+            return creator.apply(value);
+    }
     public static void main(String[] args) {
-        UnidadTiempoEnum type =  UnidadTiempoEnum.valueOf("HORAS");
-        System.out.println(type);
+        Function<Integer, Hora> creatorHora = Hora::of;
+        Function<Integer, Minuto> creatorMin = i -> Minuto.of(i);
+        Function<Integer, Segundo> creatorSeg = new Function<Integer, Segundo>(){
+            @Override
+            public Segundo apply(Integer i) {
+                return Segundo.of(i);
+            }
+        };
+
+
+        Hora hora = creator(2, creatorHora);
+        Minuto minuto = creatorMin.apply(1);
+        Segundo segundo = creatorSeg.apply(3);
+        System.out.println(hora);
+        System.out.println(minuto);
+        System.out.println(segundo);
 
     }
 }
